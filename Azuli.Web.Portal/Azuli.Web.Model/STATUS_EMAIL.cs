f48fa5881856e9e9530860a30e310874ae6da6d8
edmls35 <edmilson.santos@el2.com.br>
2016-01-05 22:41:07 -0200

    Adicionando Email Maloter

---
+++ Azuli.Web.Portal/Azuli.Web.Model/STATUS_EMAIL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Azuli.Web.Model
+{
+    public class STATUS_EMAIL
+    {
+        public int id_email { get; set; }
+        public ApartamentoModel apBloco { get; set; }
+        public int MyProperty { get; set; }
+        public int MyProperty { get; set; }
+
+
+    }
+}
